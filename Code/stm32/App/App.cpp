24d8a102849b44a0d8f614dfbaf38584e948940f
lissettecarlr <lissettecarlr@163.com>
2016-01-22 17:26:31 +0800

    导入AHRS文件，将延时改为了2ms

--- Code/stm32/App/App.cpp
+++ Code/stm32/App/App.cpp
@@ -41,12 +41,12 @@ void App::Loop()
 	mLedGreen.Toggle();
 	if(MOD_ERROR==mMPU6050.Update())
 		mCom1<<"Update Error!\r\n";
-	mCom1<<mMPU6050.GetAccRaw().x<<"\t"<<mMPU6050.GetAccRaw().y<<"\t"<<mMPU6050.GetAccRaw().z<<"\t";
-	mCom1<<mMPU6050.GetGyrRaw().x<<"\t"<<mMPU6050.GetGyrRaw().y<<"\t"<<mMPU6050.GetGyrRaw().z<<"\t";
+//	mCom1<<mMPU6050.GetAccRaw().x<<"\t"<<mMPU6050.GetAccRaw().y<<"\t"<<mMPU6050.GetAccRaw().z<<"\t";
+//	mCom1<<mMPU6050.GetGyrRaw().x<<"\t"<<mMPU6050.GetGyrRaw().y<<"\t"<<mMPU6050.GetGyrRaw().z<<"\t";
 
 	angle = AHRS::GetAngle(mMPU6050.GetAccRaw(),mMPU6050.GetGyrRaw());
 	mCom1<<angle.x<<"\t"<<angle.y<<"\t"<<angle.z<<"\r\n";
 	
-	TaskManager::DelayMs(500);
+	TaskManager::DelayMs(2);
 }
 
